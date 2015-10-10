ace9d10d771941898f4f9b26a98c636845ea0983
underdogg <drosbeds@xs4all.nl>
2015-10-10 14:22:02 +0200

    Some minor changes (app not working yet because of infinite redirect) the definition of module interface has become stricter, so the function contstruction (parameter) had to be changed to \Phalcon\DiInterface $di = null for 3 functions

--- app/frontend/Module.php
+++ app/frontend/Module.php
@@ -10,7 +10,7 @@ class Module extends ModuleConfig
     /**
      * @param \Phalcon\DiInterface $di
      */
-    public function registerServices($di) {
+    public function registerServices(\Phalcon\DiInterface $di = null) {
         parent::registerServices($di);
         $view = $di->get('view');
         $view->setVar('bLoadModuleMenu', false);
